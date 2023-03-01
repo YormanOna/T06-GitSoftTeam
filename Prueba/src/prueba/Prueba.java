e423baff9408862a062db0d99539340136846dbb
Diego Pilataxi <dapilataxi1@espe.edu.ec>
2023-03-01 01:02:14 -0500

    test

---
+++ Prueba/src/prueba/Prueba.java
@@ -0,0 +1,17 @@
+
+package prueba;
+
+/**
+ *
+ * @author Diego Pilataxi, GitSoftTeam, DCCO-ESPE
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
